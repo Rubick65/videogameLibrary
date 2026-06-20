6e182fa4b17e4e7dec05cad2df292416a97b9284
ruben <martinandraderuben@gmail.com>
2026-06-21 01:01:52 +0200

    feat: add header and register components, only visual

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login-component/login-component';
+import { RegisterComponent } from './register-component/register-component';
+import { HeaderComponent } from './components/header-component/header-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, HeaderComponent, LoginComponent, RegisterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
