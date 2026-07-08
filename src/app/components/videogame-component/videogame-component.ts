2af1a97fed0e925093b04e66e8c8c8fc80774360
ruben <martinandraderuben@gmail.com>
2026-07-08 23:52:03 +0200

    feat: add videogame component beta visuals

--- src/app/components/videogame-component/videogame-component.ts
+++ src/app/components/videogame-component/videogame-component.ts
@@ -9,19 +9,30 @@ import { IconData } from './VideogameInterface';
 })
 export class VideogameComponent {
   hover = signal(false);
+  private videogameDescriptionContainer = document.querySelector('.videogame_description_container') as HTMLDivElement;
 
   iconsData: IconData[] = [
-    { id: 1, src: 'assets/icons/likeGame.png', number: 5 },
-    { id: 2, src: 'assets/icons/likeGame.png', number: 10 },
-    { id: 3, src: 'assets/icons/likeGame.png', number: 3 },
-    { id: 4, src: 'assets/icons/likeGame.png', number: 9 },
+    { id: 1, src: 'assets/icons/likeGame.svg', number: 5 },
+    { id: 2, src: 'assets/icons/gameFav.svg', number: 10},
   ];
 
-  mouseOver(){
-    this.hover.set(true)
+  mouseOver() {
+    this.hover.set(true);
   }
 
-  mouseOut(){
-    this.hover.set(false)
+  mouseOut() {
+    this.hover.set(false);
+  }
+
+  addToLibraryIconClick(event: MouseEvent) {
+    const target  = event.target as HTMLImageElement;
+
+    if (target.src.includes('assets/icons/addToLibrary.svg')) {
+      target.src = 'assets/icons/removeFromLibrary.svg';
+    }else{
+      target.src = 'assets/icons/addToLibrary.svg';
+    }
+
+    
   }
 }
